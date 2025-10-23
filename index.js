6ac5d4b84fe2e69046963fc09e2c7869d88d41fb
Jose Rojas <mrrojas213@gmail.com>
2025-10-23 16:11:58 -0500

    Submission gives a message then goes back to home page

--- index.js
+++ index.js
@@ -39,7 +39,8 @@ app.post('/contact', async (req, res) => {
         const newContact = new Contact({ first_name, last_name, phone, email, rental_date, reference, message });
         await newContact.save();
         console.log('New Message Saved: ', newContact);
-        res.status(201).send('Message received!');
+        res.redirect('/');
+        
 
         const transporter = nodemailer.createTransport({
             service: 'gmail',
